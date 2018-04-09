0e7a54681f267c840d1584c50850f627c62e10df
Zuohan Xiahou <Xiahou@zuohans-mbp.dhcp.wustl.edu>
2018-04-09 12:47:46 -0500

    minor bug fixes

--- pbfs.cpp
+++ pbfs.cpp
@@ -6,16 +6,18 @@
 #include <vector>
 
 #include"reducer_bag.h"
+using namespace std;
 
 #define V0 0
+int * dist;
 
-void get_neighbour(int v, vector<vector<int>> list);
+vector<int> get_neighbour(int v, vector<vector<int>> list);
 void process_layer(BagView* bag, BagView* next, int d);
 void process_pennant(Pennant* p, BagView* bag, int d);
 
 int main(){
 	int v = 1000; // number of vertices
-	int* dist = new int [v];
+	dist = new int [v];
 	dist[V0] = 0;
 	for(int i = 1; i < v; i++){
 		dist[i] = 0x7fffffff;
@@ -41,7 +43,7 @@ void get_neighbour(int v, vector<vector<int>> list){
 void process_layer(BagView* bag, BagView* next, int d){
 	cilk_for(int i = 0; i < bag->get_depth(); i++){
 		if(!bag->get_value()->S[i].is_empty()){
-			process_pennant(bag->get_value()->S[i],next,d);
+			process_pennant(&(bag->get_value()->S[i]),next,d);
 		}
 	}
 }
@@ -51,7 +53,8 @@ void process_pennant(Pennant* p, BagView* bag, int d){
 		std::queue<Node*> q;
 		q.push(p->root);
 		while(q.size()){
-			Node* front = q.pop();
+			Node* front = q.back();
+			q.pop();
 			int v = front->val;
 			vector<int> adj = get_neighbour(v);
 			cilk_for(int i = 0; i < adj.size(); i++){
@@ -69,6 +72,6 @@ void process_pennant(Pennant* p, BagView* bag, int d){
 		cilk_spawn process_pennant(p, bag, d);
 		process_pennant(other, bag,d);
 		cilk_sync;
-	}	
+	}
 }
 
